c6cfc4316f5145d5178f79c06967d4b97152ec8e
Samy Ghribi <ghribi.samy@gmail.com>
2016-02-03 14:17:11 +0100

    Update build-ios.sh

--- build-ios.sh
+++ build-ios.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p builds
+./node_modules/.bin/gulp build
 ionic build ios --release --device
 cd ./platforms/ios/build/device/
 /usr/bin/xcrun -sdk iphoneos PackageApplication "$(pwd)/FlatTracker.app" -o "$(pwd)/FlatTracker.ipa"
